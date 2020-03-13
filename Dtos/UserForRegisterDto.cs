f3037df84e960cbbfc031e01478505fa88531d5d
bdafahim <badrudduza20@student.sust.edu>
2020-03-13 23:59:28 +0600

    jwt authentication

--- Dtos/UserForRegisterDto.cs
+++ Dtos/UserForRegisterDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
-    public class UserForregisterDto
+    public class UserForRegisterDto
     {
         [Required]
         public string Username { get; set; }
