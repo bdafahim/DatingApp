f138f289d37eb281334cc66af7c5dc20cbddb3a5
bdafahim <badrudduza20@student.sust.edu>
2020-04-13 13:51:15 +0600

    Adding the setMain functionality to the API

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -95,6 +95,32 @@ namespace DatingApp.API.Controllers
             }
             return BadRequest("Could not add the photo");
         }
+
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMainPhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _repo.GetUser(userId);
+
+            if (!user.Photos.Any(p => p.Id == id))
+                return Unauthorized();
+
+            var photoFromRepo = await _repo.GetPhoto(id);
+
+            if (photoFromRepo.isMain)
+                return BadRequest("This is already the main photo");
+
+            var currentMainPhoto = await _repo.GetMainPhotoForUser(userId);
+            currentMainPhoto.isMain = false;
+
+            photoFromRepo.isMain = true;
+
+            if (await _repo.SaveAll())
+                return NoContent();
+            return BadRequest("Could not set photo to main");
+        }
     }
 }
 
