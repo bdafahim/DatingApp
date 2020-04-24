a5d98606f8070979c92742a8409d8c8d192ede9e
bdafahim <badrudduza20@student.sust.edu>
2020-04-24 23:01:19 +0600

    Paginatio Implementation in the api

--- Helpers/UserParams.cs
+++ Helpers/UserParams.cs
@@ -2,6 +2,22 @@ namespace DatingApp.API.Helpers
 {
     public class UserParams
     {
-        
+        private const int MaxpageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxpageSize) ? MaxpageSize : value; }
+        }
+
+        private int myVar;
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
     }
 }
