a83e3c8b2a0476e838ef2cc217bb5c64d0c5f452
bdafahim <badrudduza20@student.sust.edu>
2020-05-01 23:50:39 +0600

    PostController Post/Get

--- Dtos/PostForCreationDto.cs
+++ Dtos/PostForCreationDto.cs
@@ -1,8 +1,16 @@
+using System;
+
 namespace DatingApp.API.Dtos
 {
     public class PostForCreationDto
     {
         public string UserName { get; set; }
         public string Status { get; set; }
+        public DateTime Created { get; set; }
+
+        public PostForCreationDto()
+        {
+            Created = DateTime.Now;
+        }
     }
 }
---
+++ Migrations/20200501172702_addPostModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class addPostModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Posts",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Posts",
+                newName: "ID");
+        }
+    }
+}
